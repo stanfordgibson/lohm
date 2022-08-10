543a2874f79b0c8d14dced157579e36bfc00a26d
Stanford Gibson <stanford.gibson@yahoo.com>
2022-08-10 15:56:34 +0200

    update

--- lohm_app.py
+++ lohm_app.py
@@ -8,14 +8,14 @@ Affordable skincare for everyone
 ''')
 
 with st.container():
-    image_col, text_col = st.columns(1,2)
+    image_col, text_col = st.columns((1,2))
     with image_col:
         st.image(image)
     with text_col:
         st.write(text)
 
 with st.container():
-    image_col, text_col = st.columns(1,2)
+    image_col, text_col = st.columns((1,2))
     with image_col:
         st.image(image2)
     with text_col:
