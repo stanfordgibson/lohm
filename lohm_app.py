69997091996dd371d41ebe37436a62ccd7746090
Stanford Gibson <stanford.gibson@yahoo.com>
2022-08-10 15:42:15 +0200

    update

--- lohm_app.py
+++ lohm_app.py
@@ -1,4 +1,25 @@
 import streamlit as st
 from PIL import Image # Load images
 
-st.write('Here it goes')
+st.markdown('# LOHM')
+st.markdown('## Land Of Milk and Honey')
+st.markdown('''
+Affordable skincare for everyone
+''')
+
+with st.container():
+    image_col, text_col = st.columns(1,2):
+    with image_col:
+        st.image(image)
+    with text_col:
+        st.write(text)
+
+with st.container():
+    image_col, text_col = st.columns(1,2):
+    with image_col:
+        st.image(image2)
+    with text_col:
+        st.write(text2)
+
+
+
